ec023e1c527722ae24041beb14705739e7a684bc
unknown <gautamn2002@Nitin-PC.(none)>
2012-04-06 16:05:29 +0530

    music band updated from site

--- view/music-band.php
+++ view/music-band.php
@@ -2,95 +2,269 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Get Your Venue Allied Services, Wedding Venues in Delhi, Wedding Farmhouses In delhi, Banquet halls in Delhi</title>
-
-<meta name=”Description” content=”Choosing the right venue for the wedding is the most difficult decision a couple comes across. We at
-getyourvenue.com make sure that all your problems are vanished and we make this arduos task an easy and fun-loving one.”>
-
-<meta name=”keyword” content=” Get Your Venue Allied Services, Wedding Venues in Delhi, Wedding Farmhouses In delhi, Banquet halls in Delhi”>
+<title>Wedding Music Bands Delhi, Live Music Bands for Weddings, Live Wedding Music Bands Delhi</title>
+<meta name="Description" content="Wedding Music Bands in Delhi can now be booked with get your venue. Live Wedding Music needs to be chosen tastefully and our live music band for weddings is mystique in its own way. Our Delhi music band for wedding would truly add the flavor of elegance to your ambience!" />
+<meta name="keyword" content="wedding music band, wedding music bands, music bands for wedding, live band in delhi, live wedding music, music bands for weddings, live music for weddings" />
 <link rel="stylesheet" type="text/css" href="../css/style.css" />
 </head>
 <body>
 <div id="wrapper">
   <div id="main">
-     <?php  require("../view/header.php") ?>
-    
+    <?php  require("../view/header.php") ?>
     <div id="contentArea">
-      <div id="leftNav">
-        <!-- Box Start -->
+      <div id="leftNavNarrow">
+      <!-- Small Left Box Start -->
+        <div class="box">
+          <div class="boxtop"><img src="/images/boxtopc1.png" align="left" alt="" /><img src="/images/boxtopc2.png" align="right" alt="" />
+            <div class="boxtopbg sml"></div>
+          </div>
+          <div class="fullboxBg">
+            <div class="boxContent">
+              <div class="boxGr" align="center">
+                <!-- Box Content Start -->
+                <script type="text/javascript"><!--
+google_ad_client = "ca-pub-4918915311035756";
+/* Side Panels */
+google_ad_slot = "6847862507";
+google_ad_width = 120;
+google_ad_height = 600;
+//-->
+</script>
+                <script type="text/javascript"
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script>
+                <!-- Box Content End -->
+              </div>
+            </div>
+          </div>
+          <div class="boxbot"><img src="/images/boxbotc3.png" align="left" alt="" /><img src="/images/boxbotc4.png" align="right" alt="" />
+            <div class="boxbotbg sml"></div>
+          </div>
+        </div>
+        <!-- Small Left Box End -->
+      </div>
+      <div id="rightNavWide">
+      <!-- Box Start -->
         <div class="box">
           <div class="boxtop"><img src="../images/boxtopc1.png" align="left" alt="" /><img src="../images/boxtopc2.png" align="right" alt="" />
             <div class="boxtopbg"></div>
           </div>
           <div class="fullboxBg">
             <div class="boxContent">
-              <div class="boxGr boxheight">
+              <div class="boxGr">
                 <!-- Box Content Start -->
+                <a name="imageGallery"></a>
                 <h3>GYV's  Mystique!</h3>
-                <p>Getyourvenue has tie-up with Delhi NCR's promising band 'Mystique' which is run by a bunch of passionate musicians and singers for whom life is beyond eating, earning and having materialistic fun. The group encompasses multi talented people from various fields of life, fairly successful in their core career such as digital marketing, industrial production, professional photography but they cut a fine balance between their core professional skill-set and god gifted and fondly nurtured music talent. These people are driven by passion of music and music is their breath and blood. Formed in early 2011, the members of the band already had great individual track records of performance. They brought synergies as a group and there has been no looking back since then.</p>
+                <div class="image-slider-small f-left" style="height:130px !important;">
+                 
+                    <img src="images/mystique_logosmall.gif" width="350" alt="Mystique The Band" />
+                </div>
+                <p>Getyourvenueâ€™s promising live music band 'Mystique' is based in delhi and is run by a bunch of passionate musicians and singers for whom life is beyond eating, earning and having materialistic fun. The wedding music band encompasses multi talented people from various fields of life, fairly successful in their core career such as digital marketing, industrial production, professional photography but they cut a fine balance between their core professional skill-set and god gifted and fondly nurtured music talent. These people are driven by passion of live music and music is their breath and blood. Formed in early 2011, the members of the wedding music band already had great individual track records of performance. They brought synergies in the band as a group and there has been no looking back since then.</p>
                 <p>Personally fond of experimental, fusion, new age music, they have a passion for going about any type of genre, repertoire and have the ability to do justice with it.</p>
                 <p>Some of the categories can be highlighted as follows:</p>
                 <table>
-				  <tr>	
-					<td width="60%">
-		                <ul>
-		                <li><p>Modern Film Music</p></li>
-						<li><p>Old Classics</p></li>
-						<li><p>Instrumental</p></li>
-						<li><p>Semi Classical Instrumental</p></li>
-						<li><p>Thematic music</p></li>
-						
-		                </ul>
-		             </td>
-		             <td style="vertical-align:top">
-		             	<ul>
-		             		<li><p>Tributes</p></li>
-		             		<li><p>Unplugged format( only acoustics, grand piano, tabla, guitar, etc)</p></li>
-		             	</ul>
-		             </td>
-		           </tr>
-		          </table> 
+                  <tr>
+                    <td width="60%"><ul>
+                        <li>
+                          <p>Modern Film Music</p>
+                        </li>
+                        <li>
+                          <p>Old Classics</p>
+                        </li>
+                        <li>
+                          <p>Instrumental</p>
+                        </li>
+                        <li>
+                          <p>Semi Classical Instrumental</p>
+                        </li>
+                        <li>
+                          <p>Thematic music</p>
+                        </li>
+                      </ul></td>
+                    <td style="vertical-align:top"><ul>
+                        <li>
+                          <p>Tributes</p>
+                        </li>
+                        <li>
+                          <p>Unplugged format( only acoustics, grand piano, tabla, guitar, etc)</p>
+                        </li>
+                      </ul></td>
+                  </tr>
+<tr>
+<td colspan="2">
+<table>
+<tr>
+<td><p>Mystique Performing at Hotel Taj Palace</p>
+<iframe width="340" height="315" style="padding:0 10px 10px 0;" src="http://www.youtube.com/embed/pjuD4qCqMkE" frameborder="0" allowfullscreen></iframe>
+</td>
+<td><p>&nbsp;</p>
+<iframe width="340" height="315" style="padding:0 10px 10px 0;" src="http://www.youtube.com/embed/md9vcw_Y5gQ" frameborder="0" allowfullscreen></iframe>
+</td>
+</tr>
+<tr>
+<td><p>Mystique Performing at Jhankar Gardens</p>
+<iframe width="340" height="315" style="padding:0 10px 10px 0;" src="http://www.youtube.com/embed/0AH7FzIPAV4" frameborder="0" allowfullscreen></iframe>
+</td>
+<td><p>&nbsp;</p>
+<iframe width="340" height="315" style="padding:0 10px 10px 0;" src="http://www.youtube.com//embed/-6Mxp05DJsA" frameborder="0" allowfullscreen></iframe>
+</td>
+</tr>
+</table>
+
+
+
+</td>
+</tr>
+                </table>
+                <b> Call our helpline +91 8800095444 to book Mystique now or fill out the contact form along side! </b>
                 <!-- Box Content End -->
-                <!--
-                <br><br><br><br><br><br><br><br>
-                -->
-        		<div id='blankBlock'></div><!--<br> -->
               </div>
             </div>
           </div>
-          <div class="boxbot"><img src="../images/boxbotc3.png" align="left" alt="" /><img src="../images/boxbotc4.png" align="right" alt="" /><img src="../images/flower.png" class="flower" alt="" />
+          <div class="boxbot"><img src="../images/boxbotc3.png" align="left" alt="" /><img src="../images/boxbotc4.png" align="right" alt="" />
+            <div class="boxbotbg"></div>
+          </div>
+        </div>
+        <!-- Box End -->
+        <!-- Box Start -->
+        <div class="box">
+          <div class="boxtop"><img src="../images/boxtopc1.png" align="left" alt="" /> <img src="../images/boxtopc2.png" align="right" alt="" />
+            <div class="boxtopbg"></div>
+          </div>
+          <div class="fullboxBg">
+            <div class="boxContent">
+              <div class="boxGr">
+                <!-- Box Content Start -->
+                <a name="bookNow"></a>
+                <h3>Get This Service Now</h3>
+                <div id='errorMessages'></div>
+                <form class="getVenue" name="bookVenueForm" method="POST">
+                  <!-- action="booking-confirmation"-->
+                  <div class="getvenue_cont">
+                    <label>Name:</label>
+                    <input type="text" name="name" class="txt-name" value="Enter Your Name" onblur="if(this.value==''){this.value='Enter Your Name'}" onfocus="if(this.value=='Enter Your Name'){this.value=''}"  />
+                  </div>
+                  <div class="getvenue_cont">
+                    <label>Email:<font color="red">*</font></label>
+                    <input type="text" name="email" class="txt-name" value="Enter your Email Id" onblur="if(this.value==''){this.value='Enter your Email Id'}" onfocus="if(this.value=='Enter your Email Id'){this.value=''}"  />
+                  </div>
+                  <div class="getvenue_cont last">
+                    <label>Contact Number:<font color="red">*</font></label>
+                    <input type="text" name="contactNumber" class="txt-name" value="Enter your Contact Number" onblur="if(this.value==''){this.value='Enter your Contact Number'}" onfocus="if(this.value=='Enter your Contact Number'){this.value=''}"  />
+                  </div>
+                  <!--<div class="getvenue_cont">
+                   <label>Preferred Date:</label>
+                    <input type="text" name="date" class="txt-name" value="Select your Date" onblur="if(this.value==''){this.value='Select your Date'}" onfocus="if(this.value=='Select your Date'){this.value=''}"  />
+                    <a href="#"><img src="../images/cal-icon.png" alt="" class="calicon" /></a> </div>
+                   -->
+                  <div class="getvenue_cont">
+                    <label>Preferred Date:</label>
+                    <input type="text" class="txt-name" name="date" value="" />
+                    <script language="JavaScript">
+
+						new tcal ({
+							// form name
+							'formname': 'bookVenueForm',
+							// input name
+							'controlname': 'date',
+						});
+						</script>
+                  </div>
+                  <div class="getvenue_cont">
+                    <label>Your Function:</label>
+                    <input type="text" name="function" class="txt-name" value="Enter your Function" onblur="if(this.value==''){this.value='Enter your Function'}" onfocus="if(this.value=='Enter your Function'){this.value=''}"  />
+                  </div>
+                  <div class="getvenue_cont last">
+                    <label>Your Budget in INR:</label>
+                    <input type="text" name="budget" class="txt-name" value="Enter your Budget" onblur="if(this.value==''){this.value='Enter your Budget'}" onfocus="if(this.value=='Enter your Budget'){this.value=''}"  />
+                  </div>
+                  <!--
+                  <div class="getvenue_cont">
+                    <center>
+                      <br>
+                      <img src="../EasyCaptcha/easycaptcha.php" />
+                    </center>
+                  </div>
+                  <div class="getvenue_cont">
+                    <label>Enter code of the image:<font color="red">*</font></label>
+                    <input type="text" class="txt-name" name="confirm_code"  />
+                  </div>
+                  -->
+                  <div class="clear"></div>
+                  <br>
+                  <div class="getvenue_cont last">
+                    <input type="image" src="../images/go-btn.png"  onClick="return submitVenueBookingForm()"/>
+                  </div>
+                  <input type="hidden" name="action" value="bookVenue"  />
+                  <!--input type="hidden" name="venueId" value="<?php echo $venueList[0]->venueId  ?>" /-->
+                </form>
+                <!-- Box Content End -->
+              </div>
+            </div>
+          </div>
+          <div class="boxbot"><img src="../images/boxbotc3.png" align="left" alt="" /> <img src="../images/boxbotc4.png" align="right" alt="" />
             <div class="boxbotbg"></div>
           </div>
         </div>
         <!-- Box End -->
       </div>
-      
-      <div id="rightNav">
-        <!-- Box Start -->
-        <div class="box">
-          <div class="boxtop"><img src="../images/boxtopc1.png" align="left" alt="" /><img src="../images/boxtopc2.png" align="right" alt="" />
-            <div class="boxtopbg sml2"></div>
-          </div>
-          <div class="fullboxBg">
-            <div class="boxContent">
-              <div class="boxGr boxheight">
-                <!-- Box Content Start -->
-                <h3>Contact Us</h3>
-                 <?php  require("../view/contactusForm.php") ?>
-                <!-- Box Content End -->
-                <p><br><br></p>
-              </div>
+    </div>
+    <br class="clear"/>
+    <!-- Box Start -->
+    <div class="box">
+      <div class="boxtop"><img src="/images/boxtopc1.png" align="left" alt="" /><img src="/images/boxtopc2.png" align="right" alt="" />
+        <div class="boxtopbg"></div>
+      </div>
+      <div class="fullboxBg">
+        <div class="boxContent">
+          <div class="boxGr">
+            <!-- Box Content Start -->
+            <div class="google-adds">
+              <script type="text/javascript"><!--
+					google_ad_client = "ca-pub-4918915311035756";
+					/* My Ad */
+					google_ad_slot = "1466171534";
+					google_ad_width = 728;
+					google_ad_height = 90;
+					//-->
+					</script>
+              <script type="text/javascript"
+					src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+					</script>
             </div>
-          </div>
-          <div class="boxbot"><img src="../images/boxbotc3.png" align="left" alt="" /><img src="../images/boxbotc4.png" align="right" alt="" /><img src="../images/flower.png" class="flower" alt="" />
-            <div class="boxbotbg sml2"></div>
+            <!-- Box Content End -->
           </div>
         </div>
-        <!-- Box End -->
       </div>
+      <div class="boxbot"><img src="/images/boxbotc3.png" align="left" alt="" /><img src="/images/boxbotc4.png" align="right" alt="" /><img src="/images/flower.png" class="flower" alt="" />
+        <div class="boxbotbg"></div>
       </div>
-      <?php  require("../view/footer.php") ?>
+    </div>
+    <!-- Box End -->
+    <?php  require("../view/footer.php") ?>
   </div>
 </div>
+<script type="text/javascript">
+    $(window).load(function() {
+        $('#slider').nivoSlider({
+		effect:'random', 
+        slices:17,
+        animSpeed:500,
+        pauseTime:6000,
+        startSlide:0, //Set starting Slide (0 index)
+        directionNav:true, //Next & Prev
+        directionNavHide:false, //Only show on hover
+        controlNav:true, //1,2,3...
+        controlNavThumbs:false, //Use thumbnails for Control Nav
+        controlNavThumbsFromRel:false, //Use image rel for thumbs
+        controlNavThumbsSearch: '.jpg', //Replace this with...
+        controlNavThumbsReplace: '_thumb.jpg', //...this in thumb Image src
+        keyboardNav:true, //Use left & right arrows
+        pauseOnHover:true, //Stop animation while hovering
+        manualAdvance:false, //Force manual transitions
+        captionOpacity:0
+		});
+    });
+    </script>
 </body>
 </html>
