4d0dc96f94b752d3cb4dd7f8e0e7a10e64f4a7e1
Nitin Gautam <gautamn2002@gmail.com>
2012-05-12 11:06:10 +0530

    report for bookings

---
+++ model/BookingInfo.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ * Created on Jul 26, 2011
+ *
+ * To change the template for this generated file go to
+ * Window - Preferences - PHPeclipse - PHP - Code Templates
+ */
+?>
+<?php
+	
+	class BookingInfo{
+		var $Id;
+		var $name;
+		var $email;
+		var $date;
+		var $functions;
+		var $contact;
+		var $budget;
+		var $bookingdate;
+	} 
+?>
